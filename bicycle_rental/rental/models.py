92d0214e2b604d13061cc386814e70b40008f8cf
Karthik <86306862+Karthikg1908@users.noreply.github.com>
2026-04-24 19:03:11 +0530

    Update models.py

--- bicycle_rental/rental/models.py
+++ bicycle_rental/rental/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Bicycle(models.Model):
+    name = models.CharField(max_length=100)
+    stock = models.IntegerField()
+
+class Rental(models.Model):
+    customer_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    rent_type = models.CharField(max_length=20)
+    rented_on = models.DateTimeField(auto_now_add=True)
